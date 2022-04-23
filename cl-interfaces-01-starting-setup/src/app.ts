b4ffc141257fecd63a6eb4278a6898ce47bd5f45
Casey <caseyjtite@gmail.com>
2022-04-23 15:29:15 -0700

    adding methods and private properties

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,10 +1,36 @@
 class Department {
   name: string;
+  private surfers: string[] = [];
   constructor(n: string) {
     this.name = n;
   }
-}
 
+  describe(this: Department) {
+    console.log(`The Department of ${this.name}`);
+  }
+
+  addSurfer(surfer: string) {
+    this.surfers.push(surfer);
+  }
+  printSurferInfo() {
+    console.log(this.surfers.length);
+    console.log(this.surfers);
+  }
+}
+// will be the class
+console.log("Department", Department);
 const surf = new Department("Surfing");
 
-console.log(surf);
+surf.describe();
+//will be an object
+console.log("Surfing", surf);
+
+// const surfCopy = { name: "Surfers", describe: surf.describe };
+
+// surfCopy.describe();
+//will also be an object;
+// console.log("Surfers", surfCopy);
+
+surf.addSurfer("Casey");
+surf.addSurfer("Owen");
+surf.printSurferInfo();
