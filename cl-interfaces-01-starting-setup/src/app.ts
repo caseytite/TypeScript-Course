07baa120414d35abbeca014a7f372fd16b4cc15a
Casey <caseyjtite@gmail.com>
2022-04-23 22:12:35 -0700

    generics

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,5 +1,6 @@
 class Surfing {
-  private surfers: string[] = [];
+  static currentYear: number = 2022;
+  protected surfers: string[] = [];
 
   constructor(private readonly id: number, public name: string) {}
 
@@ -14,6 +15,10 @@ class Surfing {
     console.log(this.surfers.length);
     console.log(this.surfers);
   }
+
+  static createSurfer(name: string) {
+    return { name };
+  }
 }
 
 // this sub class will inheirit the methods from the class it extends
@@ -23,7 +28,11 @@ class Shortboard extends Surfing {
     super(id, "Shortboard");
     this.admins = admins;
   }
+  addSurfer(surfer: string): void {
+    this.surfers.push(surfer);
+  }
 }
+
 // will be the class
 // console.log("Surf", Surfing);
 const surf = new Surfing(1, "Surf");
@@ -37,7 +46,13 @@ surf.addSurfer("Owen");
 surf.printSurferInfo();
 
 const short = new Shortboard(2, ["Casey"]);
+
 short.addSurfer("Casey");
 short.addSurfer("Owen");
 short.describe();
 console.log(short);
+const createdSurfer = Surfing.createSurfer("Ricky Bobby");
+console.log(createdSurfer);
+console.log(Surfing.currentYear);
+
+short.addSurfer("Allison");
